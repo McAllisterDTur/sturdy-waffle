f808729445f84b47b341f22c91df7ed384508751
Gonza116 <Gonza116@users.noreply.github.com>
2018-10-06 16:24:49 +0200

    Caso 5

--- Acme-Shout/src/main/java/services/ShoutService.java
+++ Acme-Shout/src/main/java/services/ShoutService.java
@@ -2,6 +2,8 @@
 package services;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.transaction.Transactional;
 
@@ -47,5 +49,21 @@ public class ShoutService {
 
 	public void save(final Shout shout) {
 		this.shoutRepository.save(shout);
+	}
+
+	public Map<String, Double> computeStatistics() {
+		Map<String, Double> result;
+		double countAll, countShort, countLong;
+
+		countAll = this.shoutRepository.countAllShouts();
+		countShort = this.shoutRepository.countShortShouts();
+		countLong = this.shoutRepository.countLongShouts();
+
+		result = new HashMap<String, Double>();
+		result.put("count.all.shouts", countAll);
+		result.put("count.short.shouts", countShort);
+		result.put("count.long.shouts", countLong);
+
+		return result;
 	}
 }
