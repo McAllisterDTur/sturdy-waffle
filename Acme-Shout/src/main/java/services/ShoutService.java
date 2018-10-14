d8f7971f42bd06974307cc5042725143bb22e27f
antonihipona <antonihipona@gmail.com>
2018-10-14 19:19:32 +0200

    Caso de estudio 4

--- Acme-Shout/src/main/java/services/ShoutService.java
+++ Acme-Shout/src/main/java/services/ShoutService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import repositories.ShoutRepository;
+import security.LoginService;
+import security.UserAccount;
 import domain.Shout;
 
 @Service
@@ -24,5 +26,24 @@ public class ShoutService {
 		result = this.shoutRepository.findAll();
 
 		return result;
+	}
+
+	public Shout create() {
+		Shout result;
+		UserAccount userAccount;
+		String username;
+
+		userAccount = LoginService.getPrincipal();
+		username = userAccount.getUsername();
+
+		result = new Shout();
+		result.setUsername(username);
+		result.setLink("");
+		result.setText("");
+		return result;
+	}
+
+	public void save(final Shout shout) {
+		this.shoutRepository.save(shout);
 	}
 }
