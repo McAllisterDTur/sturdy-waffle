4ff4635c50b9154062f8ac1bc4b59550f55131bd
McAllisterDTur <Joseph.jmlc@gmail.com>
2018-11-19 20:54:38 +0100

    Quitado el @Transactional
    Se han quitado los transactional y quitado unas queries que había implementadas.

--- Acme-Handy-Worker/src/main/java/repositories/AdministratorRepository.java
+++ Acme-Handy-Worker/src/main/java/repositories/AdministratorRepository.java
@@ -1,6 +1,5 @@
-package repositories;
 
-import javax.transaction.Transactional;
+package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import domain.Administrator;
 
 @Repository
-@Transactional
 public interface AdministratorRepository extends JpaRepository<Administrator, Integer> {
 
 }
--- Acme-Handy-Worker/src/main/java/repositories/PhaseRepository.java
+++ Acme-Handy-Worker/src/main/java/repositories/PhaseRepository.java
@@ -1,6 +1,5 @@
-package repositories;
 
-import javax.transaction.Transactional;
+package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import domain.Phase;
 
 @Repository
-@Transactional
 public interface PhaseRepository extends JpaRepository<Phase, Integer> {
 
 }
