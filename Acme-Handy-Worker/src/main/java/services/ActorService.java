09ae4632cdf644f6627c70cb07dc2778a6beafea
McAllisterDTur <Joseph.jmlc@gmail.com>
2018-11-24 14:00:28 +0100

    ActorService Creado
    Actor service hecho, con la lógica necesaria para realizar correctamente los save.
    Parece que no funciona, ya que no pasa los test.
    Esto es para que sea corregido.

--- Acme-Handy-Worker/src/main/java/services/ActorService.java
+++ Acme-Handy-Worker/src/main/java/services/ActorService.java
@@ -1,12 +1,19 @@
 
 package services;
 
+import java.util.Collection;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import repositories.ActorRepository;
+import security.Authority;
+import security.LoginService;
+import security.UserAccount;
+import utilities.AuthenticationUtility;
 import domain.Actor;
 
 @Service
@@ -18,7 +25,7 @@ public class ActorService {
 
 
 	/**
-	 * Create a new empty actor
+	 * Creates a new empty actor
 	 * 
 	 * @return actor
 	 */
@@ -28,13 +35,65 @@ public class ActorService {
 	}
 
 	/**
-	 * Save a new actor in the DB or update it
+	 * Saves a new actor in the DB or update it
+	 * If an admin update other actor, only can ban
 	 * 
-	 * @return
+	 * @param actor
+	 * @return actor
 	 */
-	public Actor save(final Actor a) {
-		final Actor save = this.actorRepository.save(a);
-		return save;
+	public Actor save(final Actor actor) {
+		Assert.isNull(actor);
+		Actor result = null;
+		if (actor.getId() != 0) {
+			final UserAccount ua = LoginService.getPrincipal();
+			final Actor ac = this.actorRepository.findOne(actor.getId());
+			if (AuthenticationUtility.checkAuthority(Authority.ADMIN)) {
+				if (ac.getAccount().equals(actor.getAccount()) && actor.getAccount().equals(ua))
+					result = this.actorRepository.save(actor);
+				else {
+					ac.setBanned(actor.getBanned());
+					result = this.actorRepository.save(ac);
+				}
+			} else {
+				Assert.isTrue(ac.getAccount().equals(actor.getAccount()));
+				Assert.isTrue(actor.getAccount().equals(ua));
+				actor.setBanned(false);
+				result = this.actorRepository.save(actor);
+			}
+		} else {
+			//TODO Por ahora, por decisión de grupo, la useracount se agrega
+			//en el controller
+			actor.setBanned(false);
+			result = this.actorRepository.save(actor);
+		}
+		return result;
+	}
+
+	/**
+	 * Get all actors from db
+	 * 
+	 * @return actors
+	 */
+	public Collection<Actor> findAll() {
+		//TODO Posee más lógica de negocio?
+		final UserAccount ua = LoginService.getPrincipal();
+		Assert.isNull(ua);
+		final Collection<Actor> result = this.actorRepository.findAll();
+		return result;
+	}
+
+	/**
+	 * Find an actor by id in the db
+	 * 
+	 * @param actorId
+	 * @return actor
+	 */
+	public Actor findOne(final int actorId) {
+		//TODO Posee más lógica de negocio?
+		final UserAccount ua = LoginService.getPrincipal();
+		Assert.isNull(ua);
+		final Actor result = this.actorRepository.findOne(actorId);
+		return result;
 	}
 
 }
