ec270e23c8576d45446ea4cdeb0f2571fa8e8215
McAllisterDTur <Joseph.jmlc@gmail.com>
2018-11-26 22:58:50 +0100

    Service y test de section completados
    Section está terminado y con todas sus pruebas realizadas

--- Acme-Handy-Worker/src/test/java/services/SectionServiceTest.java
+++ Acme-Handy-Worker/src/test/java/services/SectionServiceTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.Assert;
 
+import repositories.SectionRepository;
 import utilities.AbstractTest;
 import domain.Section;
 import domain.Tutorial;
@@ -25,10 +26,13 @@ import domain.Tutorial;
 public class SectionServiceTest extends AbstractTest {
 
 	@Autowired
-	private SectionService	sectionService;
+	private SectionService		sectionService;
 
 	@Autowired
-	private TutorialService	tutorialService;
+	private TutorialService		tutorialService;
+
+	@Autowired
+	private SectionRepository	sectionRepository;
 
 
 	@Test
@@ -67,111 +71,116 @@ public class SectionServiceTest extends AbstractTest {
 	public void FindOneGoodTest() {
 		final Collection<Tutorial> ac = this.tutorialService.findAll();
 		final Tutorial a = ac.iterator().next();
-		final Section tu = this.sectionService.findOne(a.getSections().iterator().next().getId());
-		Assert.isTrue((tu.equals(a.getSections().iterator().next())));
+		final Section sec = a.getSections().iterator().next();
+		final Section tu = this.sectionService.findOne(sec.getId());
+		Assert.isTrue(tu.equals(sec));
 	}
-	//
-	//	@Test
-	//	public void FindSectionssByTutorialGoodTest() {
-	//		final Collection<Tutorial> ac = this.tutorialService.findAll();
-	//		final Tutorial a = ac.iterator().next();
-	//		final Collection<Section> tu = this.sectionService.findAllFromTutorial(a.getId());
-	//		Assert.isTrue(!(tu.isEmpty()));
-	//	}
-	//
-	//	@Test
-	//	public void SaveNewGoodTest() {
-	//		final Collection<Tutorial> tutorial = this.tutorialService.findAll();
-	//		final Tutorial tuto = tutorial.iterator().next();
-	//		super.authenticate(tuto.getWorker().getAccount().getUsername());
-	//		final Section ac = this.sectionService.create();
-	//		ac.setNumber(1);
-	//		ac.setText("one");
-	//		ac.setTitle("two");
-	//		ac.setTutorial(tuto);
-	//		final Section tu = this.sectionService.save(ac);
-	//		ac.setId(tu.getId());
-	//		ac.setVersion(tu.getVersion());
-	//		Assert.isTrue(tu.equals(ac));
-	//		super.unauthenticate();
-	//	}
-	//
-	//	@Test
-	//	public void SaveNewBadTest() {
-	//		final Collection<Tutorial> tutorial = this.tutorialService.findAll();
-	//		final Tutorial tuto = tutorial.iterator().next();
-	//		super.authenticate("Customer1");
-	//		final Section ac = this.sectionService.create();
-	//		ac.setNumber(1);
-	//		ac.setText("one");
-	//		ac.setTitle("two");
-	//		ac.setTutorial(tuto);
-	//		Section tu = null;
-	//		try {
-	//			tu = this.sectionService.save(ac);
-	//		} catch (final Exception e) {
-	//			tu = null;
-	//		}
-	//		Assert.isNull(tu);
-	//		super.unauthenticate();
-	//	}
-	//
-	//	@Test
-	//	public void SaveEditGoodTest() {
-	//		final Collection<Tutorial> ac = this.tutorialService.findAll();
-	//		final Tutorial tu = ac.iterator().next();
-	//		super.authenticate(tu.getWorker().getAccount().getUsername());
-	//		final Section sec = tu.getSections().iterator().next();
-	//		sec.setTitle("dos");
-	//		final Section sect = this.sectionService.save(sec);
-	//		Assert.isTrue(sec.equals(sect));
-	//		super.unauthenticate();
-	//	}
-	//
-	//	@Test
-	//	public void SaveEditBadTest() {
-	//		final Collection<Tutorial> ac = this.tutorialService.findAll();
-	//		final Tutorial tu = ac.iterator().next();
-	//		super.authenticate("Customer1");
-	//		final Section sec = tu.getSections().iterator().next();
-	//		sec.setTitle("dos");
-	//		Section sect = null;
-	//		try {
-	//			sect = this.sectionService.save(sec);
-	//		} catch (final Exception e) {
-	//			sect = null;
-	//		}
-	//		Assert.isNull(sect);
-	//		super.unauthenticate();
-	//	}
-	//
-	//	@Test
-	//	public void DeleteGoodTest() {
-	//		final Collection<Tutorial> ac = this.tutorialService.findAll();
-	//		final Tutorial tu = ac.iterator().next();
-	//		super.authenticate(tu.getWorker().getAccount().getUsername());
-	//		tu.setId(0);
-	//		tu.setVersion(0);
-	//		final Tutorial tuto = this.tutorialService.save(tu);
-	//		this.tutorialService.delete(tuto);
-	//		Assert.isNull(this.tutorialService.findOne(tuto.getId()));
-	//		super.unauthenticate();
-	//	}
-	//
-	//	@Test
-	//	public void DeleteBadTest() {
-	//		final Collection<Tutorial> ac = this.tutorialService.findAll();
-	//		final Tutorial tu = ac.iterator().next();
-	//		super.authenticate(tu.getWorker().getAccount().getUsername());
-	//		final Section sec = tu.getSections().iterator().next();
-	//		Section sect = null;
-	//		try {
-	//			this.sectionService.delete(sec);
-	//		} catch (final Exception e) {
-	//			sect = null;
-	//		}
-	//		Assert.isNull(sect);
-	//		super.unauthenticate();
-	//	}
+	@Test
+	public void FindSectionssByTutorialGoodTest() {
+		final Collection<Tutorial> ac = this.tutorialService.findAll();
+		final Tutorial a = ac.iterator().next();
+		final Collection<Section> tu = this.sectionService.findAllFromTutorial(a.getId());
+		Assert.isTrue(!(tu.isEmpty()));
+	}
+
+	@Test
+	public void SaveNewGoodTest() {
+		final Collection<Tutorial> tutorial = this.tutorialService.findAll();
+		final Tutorial tuto = tutorial.iterator().next();
+		super.authenticate(tuto.getWorker().getAccount().getUsername());
+		final Section ac = this.sectionService.create();
+		ac.setNumber(3);
+		ac.setText("one");
+		ac.setTitle("two");
+		ac.setTutorial(tuto);
+		final Section tu = this.sectionService.save(ac);
+		ac.setId(tu.getId());
+		ac.setVersion(tu.getVersion());
+		Assert.isTrue(tu.equals(ac));
+		super.unauthenticate();
+	}
+
+	@Test
+	public void SaveNewBadTest() {
+		final Collection<Tutorial> tutorial = this.tutorialService.findAll();
+		final Tutorial tuto = tutorial.iterator().next();
+		super.authenticate("Customer1");
+		final Section ac = new Section();
+		ac.setNumber(1);
+		ac.setText("one");
+		ac.setTitle("two");
+		ac.setTutorial(tuto);
+		Section tu = null;
+		try {
+			tu = this.sectionService.save(ac);
+		} catch (final Exception e) {
+			tu = null;
+		}
+		Assert.isNull(tu);
+		super.unauthenticate();
+	}
+
+	@Test
+	public void SaveEditGoodTest() {
+		final Collection<Tutorial> ac = this.tutorialService.findAll();
+		final Tutorial tu = ac.iterator().next();
+		super.authenticate(tu.getWorker().getAccount().getUsername());
+		final Section sec = tu.getSections().iterator().next();
+		sec.setTitle("dos");
+		final Section sect = this.sectionService.save(sec);
+		Assert.isTrue(sec.equals(sect));
+		super.unauthenticate();
+	}
+
+	@Test
+	public void SaveEditBadTest() {
+		final Collection<Tutorial> ac = this.tutorialService.findAll();
+		final Tutorial tu = ac.iterator().next();
+		super.authenticate("Customer1");
+		final Section sec = tu.getSections().iterator().next();
+		sec.setTitle("dos");
+		Section sect = null;
+		try {
+			sect = this.sectionService.save(sec);
+		} catch (final Exception e) {
+			sect = null;
+		}
+		Assert.isNull(sect);
+		super.unauthenticate();
+	}
+
+	@Test
+	public void DeleteGoodTest() {
+		final Collection<Tutorial> ac = this.tutorialService.findAll();
+		final Tutorial tu = ac.iterator().next();
+		super.authenticate(tu.getWorker().getAccount().getUsername());
+
+		final Section acs = new Section();
+		acs.setNumber(1);
+		acs.setText("one");
+		acs.setTitle("two");
+		acs.setTutorial(tu);
+
+		final Section sect = this.sectionService.save(acs);
+		this.sectionService.delete(sect);
+		Assert.isNull(this.sectionService.findOne(sect.getId()));
+		super.unauthenticate();
+	}
+
+	@Test
+	public void DeleteBadTest() {
+		final Collection<Tutorial> ac = this.tutorialService.findAll();
+		final Tutorial tu = ac.iterator().next();
+		super.authenticate("Customer1");
+		final Section sec = tu.getSections().iterator().next();
+		Section sect = null;
+		try {
+			this.sectionService.delete(sec);
+		} catch (final Exception e) {
+			sect = null;
+		}
+		Assert.isNull(sect);
+		super.unauthenticate();
+	}
 
 }
