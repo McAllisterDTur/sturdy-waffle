0e9c86c8e434c21d46abe8aa8ee82e729bb181b5
McAllisterDTur <Joseph.jmlc@gmail.com>
2018-11-27 01:12:37 +0100

    Terminado Sponsorship
    Se ha realizado el service, repository y las pruebas para el sponsorship.

--- Acme-Handy-Worker/src/main/java/repositories/SponsorshipRepository.java
+++ Acme-Handy-Worker/src/main/java/repositories/SponsorshipRepository.java
@@ -1,7 +1,10 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Sponsorship;
@@ -9,4 +12,6 @@ import domain.Sponsorship;
 @Repository
 public interface SponsorshipRepository extends JpaRepository<Sponsorship, Integer> {
 
+	@Query("select t.sponsorships from Tutorial t where t.id = ?1")
+	Collection<Sponsorship> findAllSponsorchipFromTutorial(int tutorial);
 }
